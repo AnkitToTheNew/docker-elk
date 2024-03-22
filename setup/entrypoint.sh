12c61c0b51727074130f5e7743bb451f895d4aa3
Ankit Kaushik <klatte09>
2024-03-22 14:36:47 +0530

    changes for kibana

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -59,49 +59,49 @@ sublog 'Elasticsearch is running'
 
 log 'Waiting for initialization of built-in users'
 
-wait_for_builtin_users || exit_code=$?
+# wait_for_builtin_users || exit_code=$?
 
-if ((exit_code)); then
-	suberr 'Timed out waiting for condition'
-	exit $exit_code
-fi
+# if ((exit_code)); then
+# 	suberr 'Timed out waiting for condition'
+# 	exit $exit_code
+# fi
 
-sublog 'Built-in users were initialized'
+# sublog 'Built-in users were initialized'
 
-for role in "${!roles_files[@]}"; do
-	log "Role '$role'"
+# for role in "${!roles_files[@]}"; do
+# 	log "Role '$role'"
 
-	declare body_file
-	body_file="${BASH_SOURCE[0]%/*}/roles/${roles_files[$role]:-}"
-	if [[ ! -f "${body_file:-}" ]]; then
-		sublog "No role body found at '${body_file}', skipping"
-		continue
-	fi
+# 	declare body_file
+# 	body_file="${BASH_SOURCE[0]%/*}/roles/${roles_files[$role]:-}"
+# 	if [[ ! -f "${body_file:-}" ]]; then
+# 		sublog "No role body found at '${body_file}', skipping"
+# 		continue
+# 	fi
 
-	sublog 'Creating/updating'
-	ensure_role "$role" "$(<"${body_file}")"
-done
+# 	sublog 'Creating/updating'
+# 	ensure_role "$role" "$(<"${body_file}")"
+# done
 
-for user in "${!users_passwords[@]}"; do
-	log "User '$user'"
-	if [[ -z "${users_passwords[$user]:-}" ]]; then
-		sublog 'No password defined, skipping'
-		continue
-	fi
+# for user in "${!users_passwords[@]}"; do
+# 	log "User '$user'"
+# 	if [[ -z "${users_passwords[$user]:-}" ]]; then
+# 		sublog 'No password defined, skipping'
+# 		continue
+# 	fi
 
-	declare -i user_exists=0
-	user_exists="$(check_user_exists "$user")"
+# 	declare -i user_exists=0
+# 	user_exists="$(check_user_exists "$user")"
 
-	if ((user_exists)); then
-		sublog 'User exists, setting password'
-		set_user_password "$user" "${users_passwords[$user]}"
-	else
-		if [[ -z "${users_roles[$user]:-}" ]]; then
-			suberr '  No role defined, skipping creation'
-			continue
-		fi
+# 	if ((user_exists)); then
+# 		sublog 'User exists, setting password'
+# 		set_user_password "$user" "${users_passwords[$user]}"
+# 	else
+# 		if [[ -z "${users_roles[$user]:-}" ]]; then
+# 			suberr '  No role defined, skipping creation'
+# 			continue
+# 		fi
 
-		sublog 'User does not exist, creating'
-		create_user "$user" "${users_passwords[$user]}" "${users_roles[$user]}"
-	fi
-done
+# 		sublog 'User does not exist, creating'
+# 		create_user "$user" "${users_passwords[$user]}" "${users_roles[$user]}"
+# 	fi
+# done
